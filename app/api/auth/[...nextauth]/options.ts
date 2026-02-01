d6340fdc5afbd8ee3d7f7dd34ef49e7aaac1313b
Rahul Kumawat <rahullkumawat2527@gmail.com>
2026-02-02 00:54:08 +0530

    setup nextauth setup and created a credential authoption

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,87 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcrypt"
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/models/user.model";
+
+
+export const authOptions: NextAuthOptions = {
+    providers: [
+        CredentialsProvider({
+            id: "credentials",
+            name: "Credentials",
+
+            credentials: {
+                email: { label: "Email", type: "text", placeholder: "Enter Your Email" },
+                password: { label: "Password", type: "password" }
+            },
+
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            async authorize(credentials: any): Promise<any> {
+                await dbConnect()
+
+                try {
+                    const user = await UserModel.findOne({
+                        $or: [
+                            { email: credentials.identifier },
+                            { username: credentials.identifier }]
+                    })
+
+                    if (!user) {
+                        throw new Error("No user is found with this email")
+                    }
+
+                    // what if user exist not verified too uske liye hum yaha check lagayenge
+                    if (!user.isVerified) {
+                        throw new Error("please verify your account before login via email")
+                    }
+
+                    // agar user exist karta ha to hum fir yaha usko password check karenge 
+                    const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password)
+
+                    if (isPasswordCorrect) {
+                        return user
+                    } else {
+                        throw new Error("Incorrect Password")
+                    }
+
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                } catch (error: any) {
+                    throw new Error(error)
+
+                }
+            }
+
+        })
+    ],
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token._id = user._id?.toString()
+                token.isVerified = user.isVerified
+                token.isAcceptingMessages = user.isAcceptingMessages
+                token.username = user.username
+            }
+
+            return token
+        },
+        async session({ session, token }) {
+            if (token) {
+                session.user._id = token._id?.toString()
+                session.user.isVerified = token.isVerified
+                session.user.isAcceptingMessages = token.isAcceptingMessages
+                session.user.username = token.username
+            }
+            return session
+        }
+
+    },
+    pages: {
+        signIn: "/signin"
+    },
+    session: {
+        strategy: "jwt"
+    },
+    secret: process.env.NEXTAUTH_SECRET
+
+}
--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,25 +1,99 @@
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/models/user.model";
 import bcrypt from "bcrypt"
 import { success } from "zod";
 
-export async function POST(request:Request){
+export async function POST(request: Request) {
     await dbConnect()
 
     try {
-        const{username,email,password} = await request.json()
-        
-        
-    } catch (error) {
-        console.error("Error while registering user",error)
-        return Response.json({
-            success : false,
-            message : "Error while registering user"
-        },
-        {
-            status : 500
+        const { username, email, password } = await request.json()
+
+        const existingUserVerifiedByEmail = await UserModel.findOne({
+            username,
+            isVerified: true
+        })
+        // yaha hum user ko username aur saath hi wo verified ha ki nahi uss cheez se check kar rhe h
+        if (existingUserVerifiedByEmail) {
+            return Response.json({
+                success: false,
+                message: "username is already taken"
+            }, { status: 400 })
         }
-    )
+
+        // jabb hum user ko email se varify karte h
+        const verifyingUserByEmail = await UserModel.findOne({ email })
+
+        // generating a verify code using math object classes
+        const verifyCode = Math.floor(10000 + Math.random() * 80000).toString()
         
+        // scenerio when user is also present 
+        if (verifyingUserByEmail) {
+
+            // user is present and also verify jo here just send response that user is already present
+            if(verifyingUserByEmail.isVerified){
+                return Response.json({
+                    success : false,
+                    message : "user is already registered"
+                },{status : 400})
+            }
+
+            const hashedPassword = await bcrypt.hash(password,10)
+
+            // kya pata jo user exist karta ha to usko password yaad na ho ya kuch aur ho to hum existing passord ko update kar denge with hashedPassword
+            verifyingUserByEmail.password =  hashedPassword
+            verifyingUserByEmail.verifyCode = verifyCode
+            verifyingUserByEmail.verifyCodeExpiry = new Date(Date.now() + 3600000)
+
+            await verifyingUserByEmail.save()
+        
+            // scenerio when user is not present means new user,here we will create a new user ans store it in the db documents
+        } else {
+            const hashedPassword = await bcrypt.hash(password, 10)
+            const expiryDate = new Date()
+            expiryDate.setDate(expiryDate.getHours() + 1)
+
+            const newUser = new UserModel({
+                username,
+                email,
+                password: hashedPassword,
+                verifyCode,
+                verifyCodeExpiry: expiryDate,
+                isVerified: false,
+                isAcceptingMessage: true,
+                messages: []
+            })
+
+            newUser.save()
+
+        }
+
+        // send verification email
+        const emailResponse = await sendVerificationEmail(email, username, verifyCode)
+
+        if (!emailResponse.success) {
+            return Response.json({
+                success: false,
+                message: emailResponse.message
+            }, { status: 500 })
+        }
+
+        return Response.json({
+            success: true,
+            message: "User created successfully, Please Verify Your Email"
+        }, { status: 201 })
+
+    } catch (error) {
+        console.error("Error while registering user", error)
+        return Response.json({
+            success: false,
+            message: "Error while registering user"
+        },
+            {
+                status: 500
+            }
+        )
+
     }
 }
