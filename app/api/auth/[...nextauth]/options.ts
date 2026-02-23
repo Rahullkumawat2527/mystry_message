20f1256e462e6be1f85c0158295476f5a5457e4b
Rahul Kumawat <rahullkumawat2527@gmail.com>
2026-02-23 12:07:21 +0530

    made changes to the options file

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -7,7 +7,7 @@ import UserModel from "@/models/user.model";
 
 export const authOptions: NextAuthOptions = {
     providers: [
-        CredentialsProvider({
+        CredentialsProvider({x
             id: "credentials",
             name: "Credentials",
 
